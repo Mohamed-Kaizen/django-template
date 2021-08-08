496a36e2c5677ad0f1f35163d9b609912e41e3f4
Mohamed Nesredin <m.n.kaizen@gmail.com>
2021-08-08 18:26:15 +0300

    remove django_import_export

--- {{ cookiecutter.project_name}}/users/admin.py
+++ {{ cookiecutter.project_name}}/users/admin.py
@@ -2,32 +2,17 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.utils.translation import gettext_lazy as _
-from import_export import resources
-from import_export.admin import ExportActionModelAdmin
 
 from .forms import CustomUserChangeForm, CustomUserCreationForm
 from .models import CustomUser
 
 
-class UserResource(resources.ModelResource):
-    """ModelResource is Resource subclass for handling Django models."""
-
-    class Meta:
-        """Meta data."""
-
-        model = CustomUser
-        fields = ("username", "email", "date_joined", "last_login")
-
-        export_order = ("username", "email", "date_joined", "last_login")
-
-
 @admin.register(CustomUser)
-class CustomUserAdmin(ExportActionModelAdmin, UserAdmin):
+class CustomUserAdmin(UserAdmin):
     """Configure the users app in admin page."""
 
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
-    resource_class = UserResource
     model = CustomUser
     add_fieldsets = (
         (
